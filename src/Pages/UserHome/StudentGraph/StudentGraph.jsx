337a71c13da63fbd3a8b56313cf6cd9f63537bee
swapnilRoyWeb <swapnilroyweb@gmail.com>
2023-07-05 07:17:08 +0600

    some changes applied

--- src/Pages/UserHome/StudentGraph/StudentGraph.jsx
+++ src/Pages/UserHome/StudentGraph/StudentGraph.jsx
@@ -3,84 +3,86 @@ import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Resp
 
 const StudentGraph = () => {
 
-    const data = [
-        {
-          name: 'January',
-          present: 4000,
-          absent: 2400,
-          amt: 2400,
-        },
-        {
-          name: 'February',
-          present: 3000,
-          absent: 1398,
-          amt: 2210,
-        },
-        {
-          name: 'March',
-          present: 2000,
-          absent: 9800,
-          amt: 2290,
-        },
-        {
-          name: 'April',
-          present: 2780,
-          absent: 3908,
-          amt: 2000,
-        },
-        {
-          name: 'May',
-          present: 1890,
-          absent: 4800,
-          amt: 2181,
-        },
-        {
-          name: 'June',
-          present: 2390,
-          absent: 3800,
-          amt: 2500,
-        },
-        {
-          name: 'July',
-          present: 3490,
-          absent: 4300,
-          amt: 2100,
-        },
-        {
-          name: 'August',
-          present: 3490,
-          absent: 4300,
-          amt: 2100,
-        },
-        {
-          name: 'September',
-          present: 3005,
-          absent: 4300,
-          amt: 2100,
-        },
-        {
-          name: 'October',
-          present: 3490,
-          absent: 4300,
-          amt: 2100,
-        },
-        {
-          name: 'November',
-          present: 3490,
-          absent: 4300,
-          amt: 2100,
-        },
-        {
-          name: 'December',
-          present: 3490,
-          absent: 4300,
-          amt: 2100,
-        },
-      ];
+  const data = [
+    {
+      name: 'January',
+      present: 4000,
+      absent: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'February',
+      present: 3000,
+      absent: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'March',
+      present: 2000,
+      absent: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'April',
+      present: 2780,
+      absent: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'May',
+      present: 1890,
+      absent: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'June',
+      present: 2390,
+      absent: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'July',
+      present: 3490,
+      absent: 4300,
+      amt: 2100,
+    },
+    {
+      name: 'August',
+      present: 3490,
+      absent: 4300,
+      amt: 2100,
+    },
+    {
+      name: 'September',
+      present: 3005,
+      absent: 4300,
+      amt: 2100,
+    },
+    {
+      name: 'October',
+      present: 3490,
+      absent: 4300,
+      amt: 2100,
+    },
+    {
+      name: 'November',
+      present: 3490,
+      absent: 4300,
+      amt: 2100,
+    },
+    {
+      name: 'December',
+      present: 3490,
+      absent: 4300,
+      amt: 2100,
+    },
+  ];
 
 
-    return (
-        <ResponsiveContainer minWidth="80%" maxWidth='100%' minHeight="30%" maxHeight='80%'>
+  return (
+    <>
+      <h1 className='text-3xl text-center font-bold bg-indigo-300 py-3 rounded-full border-x-4 border-black'>Student Attendance</h1>
+      <ResponsiveContainer minWidth="80%" maxWidth='100%' minHeight="30%" maxHeight='80%'>
         <BarChart
           width={500}
           height={300}
@@ -100,7 +102,8 @@ const StudentGraph = () => {
           <Bar dataKey="absent" stackId="a" fill="#E21717" />
         </BarChart>
       </ResponsiveContainer>
-    );
+    </>
+  );
 };
 
 export default StudentGraph;
--- src/Pages/UserHome/StudentTotal/StudentTotal.jsx
+++ src/Pages/UserHome/StudentTotal/StudentTotal.jsx
@@ -3,7 +3,7 @@ import Tilt from 'react-parallax-tilt';
 
 const StudentTotal = () => {
     return (
-        <div className='flex flex-col lg:flex-row justify-center gap-10 h-fit text-center mt-5 xl:mt-10'>
+        <div className='flex flex-col lg:flex-row justify-center gap-10 h-fit text-center mt-5 xl:mt-10 mb-10'>
             <Tilt>
                 <div className="stats shadow-2xl shadow-black w-fit py-3 px-2 hover:bg-indigo-500 hover:text-white">
 
