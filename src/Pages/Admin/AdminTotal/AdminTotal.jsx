6281869bb045f006364886ddda556103b2595752
swapnilRoyWeb <swapnilroyweb@gmail.com>
2023-07-19 20:41:11 +0600

    some improvement added to admin dashboard ui

--- src/Pages/Admin/AdminTotal/AdminTotal.jsx
+++ src/Pages/Admin/AdminTotal/AdminTotal.jsx
@@ -14,7 +14,15 @@ const AdminTotal = () => {
             <div className="stats shadow-2xl shadow-black w-60 text-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 hover:text-white duration-300 ">
 
                 <div className="stat">
-                    <div className="stat-title text-white">Total Courses</div>
+                    <div className="stat-title text-white">Total Batch</div>
+                    <div className="stat-value">10</div>
+                </div>
+
+            </div>
+            <div className="stats shadow-2xl shadow-black w-60 text-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 hover:text-white duration-300 ">
+
+                <div className="stat">
+                    <div className="stat-title text-white">Total Classes</div>
                     <div className="stat-value">89,400</div>
                 </div>
 
--- src/Pages/Admin/Income&ExpenseGraph/IncomeGraph.jsx
+++ src/Pages/Admin/Income&ExpenseGraph/IncomeGraph.jsx
@@ -8,42 +8,84 @@ const IncomeGraph = () => {
             name: 'January',
             expense: 4000,
             income: 2400,
+            profit: 2000,
             amt: 2400,
         },
         {
             name: 'February',
             expense: 3000,
             income: 1398,
+            profit: 2000,
             amt: 2210,
         },
         {
             name: 'March',
             expense: 2000,
             income: 9800,
+            profit: 2000,
             amt: 2290,
         },
         {
             name: 'April',
             expense: 2780,
             income: 3908,
+            profit: 2000,
             amt: 2000,
         },
         {
             name: 'May',
             expense: 1890,
             income: 4800,
+            profit: 2000,
             amt: 2181,
         },
         {
             name: 'June',
             expense: 2390,
             income: 3800,
+            profit: 2000,
             amt: 2500,
         },
         {
             name: 'July',
             expense: 3490,
             income: 4300,
+            profit: 2000,
+            amt: 2100,
+        },
+        {
+            name: 'August',
+            expense: 3490,
+            income: 4300,
+            profit: 2000,
+            amt: 2100,
+        },
+        {
+            name: 'September',
+            expense: 3490,
+            income: 4300,
+            profit: 2000,
+            amt: 2100,
+        },
+        {
+            name: 'October',
+            expense: 3490,
+            income: 4300,
+            profit: 2000,
+            amt: 2100,
+        },
+        {
+            name: 'November',
+            expense: 3490,
+            income: 4300,
+            profit: 2000,
+            amt: 2100,
+        },
+        {
+            name: 'December',
+            expense: 3490,
+            income: 4300,
+            profit: 2000,
             amt: 2100,
         },
     ];
@@ -67,8 +109,9 @@ const IncomeGraph = () => {
                 <YAxis />
                 <Tooltip />
                 <Legend />
-                <Bar dataKey="income" fill="#8884d8" />
-                <Bar dataKey="expense" fill="#82ca9d" />
+                <Bar dataKey="income" fill="#82ca9d" />82ca9d
+                <Bar dataKey="expense" fill="#8884d8" />
+                <Bar dataKey="profit" fill="#E8BD0D" />
             </BarChart>
         </ResponsiveContainer>
 
