437fea379d7fbea9f3a9ff9b38989cfea032793b
swapnilRoyWeb <swapnilroyweb@gmail.com>
2024-08-03 17:25:01 +0600

    some changes to admin dashboard for backend purpose

--- src/Pages/Admin/ManageUsers/ManageUsers.jsx
+++ src/Pages/Admin/ManageUsers/ManageUsers.jsx
@@ -5,17 +5,16 @@ import Swal from 'sweetalert2';
 import { FaPlus } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 const ManageUsers = () => {
-    
-    const {data: user = [] , refetch} = useQuery(['user'],async()=>
-    {
-        const res = await fetch(' https://intern-first-server-farjanaakterlaila.vercel.app/user')
-        return res.json();
-    })
-    const [selectedUsers, setSelectedUsers] = useState([]); 
+
+  const { data: user = [], refetch } = useQuery(['user'], async () => {
+    const res = await fetch(' https://intern-first-server-farjanaakterlaila.vercel.app/user')
+    return res.json();
+  })
+  const [selectedUsers, setSelectedUsers] = useState([]);
 
   const handleMakeStudent = (user) => {
     if (user.role === 'Student' || selectedUsers.includes(user._id)) {
-      return; 
+      return;
     }
 
     fetch(` https://intern-first-server-farjanaakterlaila.vercel.app/user/student/${user._id}`, {
@@ -39,7 +38,7 @@ const ManageUsers = () => {
 
   const handleMakeInstr = (user) => {
     if (user.role === 'Instructor' || selectedUsers.includes(user._id)) {
-      return; 
+      return;
     }
 
     fetch(` https://intern-first-server-farjanaakterlaila.vercel.app/user/instructor/${user._id}`, {
@@ -63,7 +62,7 @@ const ManageUsers = () => {
 
   const handleMakeEmp = (user) => {
     if (user.role === 'Employee' || selectedUsers.includes(user._id)) {
-      return; 
+      return;
     }
 
     fetch(` https://intern-first-server-farjanaakterlaila.vercel.app/user/employee/${user._id}`, {
@@ -85,80 +84,115 @@ const ManageUsers = () => {
       });
   };
 
+  const handleMakeAdmin = (user) => {
+    if (user.role === 'Admin' || selectedUsers.includes(user._id)) {
+      return;
+    }
+
+    fetch(` https://intern-first-server-ashy.vercel.app/user/admin/${user._id}`, {
+      method: 'PATCH',
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: `${user.name} is an Admin Now!`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
+
   const UserSelection = (user) => {
     if (selectedUsers.includes(user._id)) {
-      setSelectedUsers(selectedUsers.filter((id) => id !== user._id)); 
+      setSelectedUsers(selectedUsers.filter((id) => id !== user._id));
     } else {
-      setSelectedUsers([...selectedUsers, user._id]); 
+      setSelectedUsers([...selectedUsers, user._id]);
     }
   };
 
-    return (
+  return (
     <div className="mx-5">
-    <div className=" bg-indigo-300 mx-5 py-3 rounded-full border-x-4 border-black border-b-2 w-full flex items-center justify-around my-5">
-      <h3 className="text-4xl font-bold">Users</h3>
-      <Link to='/adminDashboard/adduser'><button className='btn btn-primary flex'><FaPlus/>Add User</button></Link>
-    </div>
-    <div className="overflow-x-auto">
-      <table className="table w-full text-xl">
-        <thead className="text-xl text-center">
-          <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Role</th>
-            
-          </tr>
-        </thead>
-        <tbody className="text-center">
-          {user.map((item, index) => (
-            <tr key={item._id}>
-              <td>{index + 1}</td>
-              <td>{item.name}</td>
-              <td>{item.email}</td>
-              <td className='flex flex-col items-center justify-center gap-3'>
-                {item.role === 'Student' ? (
-                  'Student'
-                ) : (
-                  <button
-                    onClick={() => handleMakeStudent(item)}
-                    className="btn btn-sm btn-ghost bg-indigo-700 text-white"
-                    disabled={selectedUsers.includes(item._id) || item.role === 'Instructor' || item.role === 'Employee'}
-                  >
-                    Student
-                  </button>
-                )}
-                {item.role === 'Instructor' ? (
-                  'Instructor'
-                ) : (
-                  <button
-                    onClick={() => handleMakeInstr(item)}
-                    className="btn btn-sm btn-ghost bg-indigo-700 text-white"
-                    disabled={selectedUsers.includes(item._id) || item.role === 'Student'||item.role === 'Employee'}
-                  >
-                    Instructor
-                  </button>
-                )}
-                {item.role === 'Employee' ? (
-                  'Employee'
-                ) : (
-                  <button
-                    onClick={() => handleMakeEmp(item)}
-                    className="btn btn-sm btn-ghost bg-indigo-700 text-white"
-                    disabled={selectedUsers.includes(item._id) || item.role === 'Student'||item.role === 'Instructor'}
-                  >
-                    Employee
-                  </button>
-                )}
-              </td>
-              
+      <div className=" bg-indigo-300 mx-5 py-3 rounded-full border-x-4 border-black border-b-2 w-full flex items-center justify-around my-5">
+        <h3 className="text-4xl font-bold">Users</h3>
+        <Link to='/adminDashboard/adduser'><button className='btn btn-primary flex'><FaPlus />Add User</button></Link>
+      </div>
+      <div className="overflow-x-auto">
+        <table className="table w-full text-xl">
+          <thead className="text-xl text-center">
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className="text-center">
+            {user.map((item, index) => (
+              <tr key={item._id}>
+                <td>{index + 1}</td>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td className='flex flex-col items-center justify-center gap-3'>
+                  {item.role === 'Student' ? (
+                    'Student'
+                  ) : (
+                    <button
+                      onClick={() => handleMakeStudent(item)}
+                      className="btn btn-sm btn-ghost bg-indigo-700 text-white"
+                      disabled={selectedUsers.includes(item._id) || item.role === 'Instructor' || item.role === 'Employee' || item.role === 'Admin'}
+                    >
+                      Student
+                    </button>
+                  )}
+                  {item.role === 'Instructor' ? (
+                    'Instructor'
+                  ) : (
+                    <button
+                      onClick={() => handleMakeInstr(item)}
+                      className="btn btn-sm btn-ghost bg-indigo-700 text-white"
+                      disabled={selectedUsers.includes(item._id) || item.role === 'Student' || item.role === 'Employee' || item.role === 'Admin'}
+                    >
+                      Instructor
+                    </button>
+                  )}
+                  {item.role === 'Employee' ? (
+                    'Employee'
+                  ) : (
+                    <button
+                      onClick={() => handleMakeEmp(item)}
+                      className="btn btn-sm btn-ghost bg-indigo-700 text-white"
+                      disabled={selectedUsers.includes(item._id) || item.role === 'Student' || item.role === 'Instructor' || item.role === 'Admin'}
+                    >
+                      Employee
+                    </button>
+                  )}
+                  {item.role === 'Admin' ? (
+                    'Admin'
+                  ) : (
+                    <button
+                      onClick={() => handleMakeAdmin(item)}
+                      className="btn btn-sm btn-ghost bg-indigo-700 text-white"
+                      disabled={selectedUsers.includes(item._id) || item.role === 'Student' || item.role === 'Instructor' || item.role === 'Employee' }
+                    >
+                      Admin
+                    </button>
+                  )}
+                </td>
+
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  </div>
-    );
+  );
 };
 
 export default ManageUsers;
--- src/Pages/Shared/Sidebar/Sidebar.jsx
+++ src/Pages/Shared/Sidebar/Sidebar.jsx
@@ -8,11 +8,18 @@ import { BiSolidReport } from "react-icons/bi";
 import { RiLogoutCircleLine } from "react-icons/ri";
 import Swal from 'sweetalert2';
 import { PiArrowFatLineDownFill } from "react-icons/pi";
+import { useQuery } from '@tanstack/react-query';
 
 const Sidebar = () => {
     const { user, logout } = useContext(AuthContext);
     const [isStudentUser, setIsStudentUser] = useState(true); // Default to student user
     const [isAdmin, setIsAdmin] = useState(false); // Check if user is admin
+
+    // const { data: users = [], refetch } = useQuery(['user'], async () => {
+    //     const res = await fetch(' https://intern-first-server-ashy.vercel.app/user')
+    //     return res.json();
+    //   })
+    //   console.log(users);
 
     const handleLogout = () => {
         logout()
@@ -49,7 +56,7 @@ const Sidebar = () => {
                 </>
             )}
 
-            {/* {isAdmin && (
+            {/* {isAdmin && (`
                 <li>
                     <Link to='/adminDashboard/adminHome' className='flex items-center gap-3 hover:bg-indigo-500 hover:text-white py-1 px-5 rounded-lg'>
                         <span className='text-lg font-medium'>Admin Panel</span>
@@ -63,7 +70,7 @@ const Sidebar = () => {
 
     useEffect(() => {
         // Fetch data from the provided URL
-        fetch(` https://intern-first-server-farjanaakterlaila.vercel.app/post-toy?email=${user?.email}`)
+        fetch(`https://intern-first-server-ashy.vercel.app/user/admin/${user?.email}`)
             .then((response) => response.json())
             .then((data) => {
                 console.log(data)
@@ -72,7 +79,7 @@ const Sidebar = () => {
                 }
                 console.log(user)
                 // Check if the user is an admin based on their username and password
-                if (user?.email === 'admin@gmail.com') {
+                if (data.role === 'Admin') {
                     //setIsAdmin(true);
                     navigate('/adminDashboard/adminHome')
                 }
@@ -80,7 +87,7 @@ const Sidebar = () => {
             .catch((error) => {
                 console.error('Error fetching data:', error);
             });
-    }, [user]);
+    }, []);
 
     return (
         <div data-aos="fade-right" data-aos-duration="3000" className='max-h-screen w-fit bg-gradient-to-t from-[#0093E9] to-[#80D0C7] bg-opacity-50 px-5 lg:px-0 py-0 lg:py-5 rounded-3xl text-start mb-5 lg:mb-8 mt-0 lg:mt-8 hidden lg:flex border-x-8 lg:border-x-0  lg:border-y-8 border-black'>
